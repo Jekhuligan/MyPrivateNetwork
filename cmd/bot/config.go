75720013cca4db916eb5fed7fc6b6ec6f63f7927
Eveny Somov <jekhuligan@gmail.com>
2025-01-22 22:51:37 +0500

    add bot bug fix

--- cmd/bot/config.go
+++ cmd/bot/config.go
@@ -1,6 +1,7 @@
 package main
 
 const (
+	BOT_TOKEN  = "7741671258:AAEE5aKeN0CP_ygO0yyOxKxOYPxx_4ZDjgA" // Замените на ваш токен от @BotFather
 	SERVER_URL = "https://116.203.117.243:30249/insurmountablemountain"
 	USERNAME   = "tay"
 	PASSWORD   = "tibmox-qAdmog-1xytba"
