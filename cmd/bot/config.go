4c794004b43ab19170cded0247914402070b53ee
Eveny Somov <jekhuligan@gmail.com>
2025-01-24 17:21:06 +0500

    Enhance bot configuration: Added PAYMENT_TOKEN and SUBSCRIPTION_PRICE constants for payment processing. Updated comments for clarity.

--- cmd/bot/config.go
+++ cmd/bot/config.go
@@ -1,8 +1,12 @@
 package main
 
 const (
-	BOT_TOKEN  = "7741671258:AAEE5aKeN0CP_ygO0yyOxKxOYPxx_4ZDjgA" // Замените на ваш токен от @BotFather
-	SERVER_URL = "https://116.203.117.243:30249/insurmountablemountain"
-	USERNAME   = "tay"
-	PASSWORD   = "tibmox-qAdmog-1xytba"
+	BOT_TOKEN     = "7741671258:AAEE5aKeN0CP_ygO0yyOxKxOYPxx_4ZDjgA" // Замените на ваш токен от @BotFather
+	SERVER_URL    = "https://116.203.117.243:30249/insurmountablemountain"
+	USERNAME      = "tay"
+	PASSWORD      = "tibmox-qAdmog-1xytba"
+	PAYMENT_TOKEN = "2051251535:TEST:OTk5MDA4ODgxLTAwNQ" // Токен для платежей от BotFather
+
+	// Цены в копейках/центах
+	SUBSCRIPTION_PRICE = 5000 // 50 рублей
 )
